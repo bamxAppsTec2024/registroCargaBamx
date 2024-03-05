2d6839cbb6b4d559bbfc28666bf97eba280408bc
Polo1017 <A01253696@itesm.mx>
2024-03-05 02:31:10 -0700

    Comment All for merge purposes

--- screens/Historial.js
+++ screens/Historial.js
@@ -12,41 +12,49 @@ import { useEffect, useState } from "react/cjs/react.production.min";
 import Tabla from "../components/Tabla";
 
 const Historial = () => {
-  const [donativo, setDonativo] = React.useState([]);
+/* 
+const [donativo, setDonativo] = React.useState([]);
 
-  React.useEffect(() => {
-    const collectionRef = collection(getData, "donativo");
-    const q = query(collectionRef, where("cantidadCarga", ">", "0"));
+React.useEffect(() => {
+  const collectionRef = collection(getData, "donativo");
+  const q = query(collectionRef, where("cantidadCarga", ">", "0"));
 
-    const unsuscribe = onSnapshot(q, (querySnapshot) => {
-      setDonativo(
-        querySnapshot.docs.map((doc) => ({
-          id: doc.id,
-          cantidadCarga: doc.data().cantidadCarga,
-          cargaCiega: doc.data().cargaCiega,
-          conductor: doc.data().conductor,
-          donante: doc.data().donante,
-          donativo: doc.data().donativo,
-          fecha: doc.data().fecha,
-          hayDesperdicio: doc.data().hayDesperdicio,
-          porcentajeDesperdicio: doc.data().porcentajeDesperdicio,
-          razonDesperdicio: doc.data().razonDesperdicio,
-          tipoCarga: doc.data().tipoCarga,
-          uriFoto: doc.data().uriFoto,
-        })
-        )
-      )});
+  const unsuscribe = onSnapshot(q, (querySnapshot) => {
+    setDonativo(
+      querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        cantidadCarga: doc.data().cantidadCarga,
+        cargaCiega: doc.data().cargaCiega,
+        conductor: doc.data().conductor,
+        donante: doc.data().donante,
+        donativo: doc.data().donativo,
+        fecha: doc.data().fecha,
+        hayDesperdicio: doc.data().hayDesperdicio,
+        porcentajeDesperdicio: doc.data().porcentajeDesperdicio,
+        razonDesperdicio: doc.data().razonDesperdicio,
+        tipoCarga: doc.data().tipoCarga,
+        uriFoto: doc.data().uriFoto,
+      })
+      )
+    )});
 
-    return unsuscribe;
-  }, []);
+  return unsuscribe;
+}, []);
+*/
+  
+
+
 
   return (
     <SafeAreaView>
+
+      {/* 
       <ScrollView>
         {donativo.map((donativo) => 
           (<Tabla {...donativo}/>)
         )}
       </ScrollView>
+      */}
     </SafeAreaView>
   );
 };
