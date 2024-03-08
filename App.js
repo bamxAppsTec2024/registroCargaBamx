c71609afc6cd7f597e8c7b74ba6925d3aa56322c
Silvana-Ruiz <silvanadorita@hotmail.com>
2024-03-07 23:57:21 -0700

    fix styles form and Status bar

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, StatusBar } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from './components/Tabs';
 import { PaperProvider } from 'react-native-paper';
@@ -7,6 +7,10 @@ export default function App() {
   return (
     <PaperProvider>
       <NavigationContainer style={styles.container}>
+        <StatusBar
+          backgroundColor="#f2f2f2"
+          barStyle={'dark-content'}
+        />
         <Tabs />
       </NavigationContainer>
     </PaperProvider>
